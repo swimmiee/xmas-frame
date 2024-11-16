e686766b146c45aad9f94d29578d8393bfd8e19e
Sooyoung Lee <dannylisa67@gmail.com>
2024-11-17 04:00:47 +0700

    polygon contracts

--- src/configs.ts
+++ src/configs.ts
@@ -6,12 +6,12 @@
 // Gnosis	eip155:100
 // Optimism	eip155:10
 // Zora	eip155:7777777
-// Polygon	eip155:137
 // Sepolia	eip155:11155111
 // Arbitrum Sepolia	eip155:421614
 // Base Sepolia	eip155:84532
 // Optimism Sepolia	eip155:11155420
 
+const Polygon	= "eip155:137" as const;
 const Sepolia = "eip155:11155111" as const;
 const BaseSepolia = "eip155:84532" as const;
 
@@ -23,6 +23,16 @@ export const CONFIGS = {
     sepolia: {
       chainId: Sepolia,
       rpcUrl: "https://rpc.sepolia.org",
+    },
+    polygon: {
+      chainId: Polygon,
+      rpcUrl: "https://polygon.llamarpc.com",
+      contracts: {
+        owner: '0x22E4Ee2e606716d9CCB0e987e77b3c9b10c8D45E',
+        tree: '0x7141A97ca4ECEb87550f828108E1852cC3BD6f04',
+        XMAS: '0x26Da6a798BCC4595eAA46f7c17295384f56725d1',
+        nft: '0x54F52bE0AE22e5Cc4a3B87C4992ba50e842df990'
+      }
     },
     base: {
       chainId: BaseSepolia,
