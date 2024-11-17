144f5c138bbd4f58a4bf122a4fca94535116d40c
Sooyoung Lee <dannylisa67@gmail.com>
2024-11-17 09:35:01 +0700

    update polygon contracts

--- src/configs.ts
+++ src/configs.ts
@@ -11,7 +11,7 @@
 // Base Sepolia	eip155:84532
 // Optimism Sepolia	eip155:11155420
 
-const Polygon	= "eip155:137" as const;
+const Polygon = "eip155:137" as const;
 const Sepolia = "eip155:11155111" as const;
 const BaseSepolia = "eip155:84532" as const;
 
@@ -28,21 +28,21 @@ export const CONFIGS = {
       chainId: Polygon,
       rpcUrl: "https://polygon.llamarpc.com",
       contracts: {
-        owner: '0x22E4Ee2e606716d9CCB0e987e77b3c9b10c8D45E',
-        tree: '0x7141A97ca4ECEb87550f828108E1852cC3BD6f04',
-        XMAS: '0x26Da6a798BCC4595eAA46f7c17295384f56725d1',
-        nft: '0x54F52bE0AE22e5Cc4a3B87C4992ba50e842df990'
-      }
+        owner: "0x22E4Ee2e606716d9CCB0e987e77b3c9b10c8D45E",
+        tree: "0xeD308f47A6246E90BE419b318F46aB32b31182Da",
+        XMAS: "0xe8FFD02481D94E01d65DbF0887180637186944A5",
+        nft: "0x77DA77d38d9263fC0b0830b2BBd46f8f18674d95",
+      },
     },
     base: {
       chainId: BaseSepolia,
       rpcUrl: "https://sepolia.base.org",
       contracts: {
-        owner: '0x22E4Ee2e606716d9CCB0e987e77b3c9b10c8D45E',
-        tree: '0x7141A97ca4ECEb87550f828108E1852cC3BD6f04',
-        XMAS: '0x26Da6a798BCC4595eAA46f7c17295384f56725d1',
-        nft: '0x54F52bE0AE22e5Cc4a3B87C4992ba50e842df990'
-      }
+        owner: "0x22E4Ee2e606716d9CCB0e987e77b3c9b10c8D45E",
+        tree: "0x7141A97ca4ECEb87550f828108E1852cC3BD6f04",
+        XMAS: "0x26Da6a798BCC4595eAA46f7c17295384f56725d1",
+        nft: "0x54F52bE0AE22e5Cc4a3B87C4992ba50e842df990",
+      },
     },
   },
 
